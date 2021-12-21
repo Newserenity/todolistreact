9837f2bb32f004b78956dcad814ec1a014da654d
Jayden Jang <jayden@Jaydens-MacBook-Air.local>
2021-12-21 19:47:54 +0900

    add div

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [loding, setLoding] = useState(true);
+  const [Movies, setMovies] = useState([]);
+
+  const getMovies = async() => {
+    const response = await fetch(
+        'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
+      );
+    const json = await response.json();
+
+    setMovies(json.data.movies);
+    setLoding(false);
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+
+
+  // const getMovies = async () => {
+  //   const json = await(
+  //     await fetch(
+  //       "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+  //     )
+  //   ).json();
+  //   setMovies(json.data.movies);
+  //   setLoding(false);
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        loding ? <h1>loding</h1> : null
+      }
     </div>
   );
 }
