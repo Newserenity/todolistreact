af97227b0b036cbb65fb24737dbd62150c345c08
Jayden Jang <jayden@Jaydens-MacBook-Air.local>
2021-12-22 12:08:25 +0900

    react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, { useState, useEffect } from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
-
-  return(
-    null
-  )
-
+  return (
+    <Router>
+      <Routes>
+        <Route path="/abot-us">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
